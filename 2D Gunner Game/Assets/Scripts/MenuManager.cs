00304a6f47c06dc037a1df732beb947d15cb910a
Garrett May <maygarrett@hotmail.com>
2018-10-03 15:08:28 -0400

    [Update] - Main menu button fix, organised tile artwork

---
+++ 2D Gunner Game/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+
+    public void GoToScene(int sceneIndexNumber)
+    {
+        SceneManager.instance.GoToScene(sceneIndexNumber);
+    }
+}
